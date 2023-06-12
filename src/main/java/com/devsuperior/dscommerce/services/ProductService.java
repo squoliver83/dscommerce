f4632bdba506b7a0b3b15bbbd4fba7fbd073da20
Samuel Quaresma <squoliver@gmail.com>
2023-06-11 23:00:31 -0300

    Custom search by name

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -28,8 +28,8 @@ public class ProductService {
     }
 
     @Transactional(readOnly = true)
-    public Page<ProductDTO> findAll(Pageable pageable) {
-        Page<Product> result = repository.findAll(pageable);
+    public Page<ProductDTO> findAll(String name, Pageable pageable) {
+        Page<Product> result = repository.searchbyName(name, pageable);
         return result.map(product -> new ProductDTO(product));
     }
 
